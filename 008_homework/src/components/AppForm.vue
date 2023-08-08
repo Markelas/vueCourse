2dd470584dbedb18ec55b21af19ad02371835ce0
Markelas <star.wars.bat@yandex.ru>
2023-08-08 13:24:42 +0500

    Add: validate for textarea

--- 008_homework/src/components/AppForm.vue
+++ 008_homework/src/components/AppForm.vue
@@ -16,8 +16,7 @@
       <textarea v-model="value" id="value" rows="3"></textarea>
       <!--Создаём модель введенного значения-->
     </div>
-
-    <button class="btn primary">Добавить</button>
+    <button class="btn" :disabled="value.length < 3">Добавить</button>
   </form>
 </template>
 
@@ -28,19 +27,33 @@ export default {
       value: "",
       type: "title",
       id: 0,
+      valid: false,
     };
   },
+  emits: ["block-added"], //Событие на добавление блоков
   methods: {
     submitHandler() {
       //При нажатии на кнопку эмитим событие добавления блока на страницу резюме
-      this.$emit("block-added", {
-        type: this.type,
-        value: this.value,
-        id: Math.random(1, 1000),
-      });
-      console.log(this.id);
-      this.value = "";
-      this.type = "title";
+      if (this.validTextArea()) {
+        //Если форма прошла валидацию, то будет совершаться действие
+        this.$emit("block-added", {
+          type: this.type,
+          value: this.value,
+          id: Math.random(1, 1000),
+        });
+        this.value = "";
+        this.type = "title";
+        console.log("submit");
+      }
+    },
+    validTextArea() {
+      //Если в textarea написано меньше 3 символов, то форма не валидна
+      if (this.value.length < 3) {
+        this.valid = false;
+      } else {
+        this.valid = true;
+      }
+      return this.valid;
     },
   },
 };
